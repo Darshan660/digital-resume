ab1de1caa2f92b63ddc5d9964e074e127c815329
Darshan660 <85669807+Darshan660@users.noreply.github.com>
2023-05-15 18:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ with col2:
 
 # Add Twitter link
 with col3:
-    st.markdown('<a href="https://wa.me/917710020979?text=Hello%20there,%20I%20thanks%20for%20connecting!" style="text-decoration:none;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" alt="Twitter" width="40" height="40" style="display:block;margin-left:auto;margin-right:auto;"></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://wa.me/917710020979?text=Hello%20there,%20thanks%20for%20connecting!" style="text-decoration:none;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" alt="Twitter" width="40" height="40" style="display:block;margin-left:auto;margin-right:auto;"></a>', unsafe_allow_html=True)
     st.markdown('<p style="text-align:center;font-size:16px;margin-top:10px;">WhatsApp</p>', unsafe_allow_html=True)
 
 # Add Email link
